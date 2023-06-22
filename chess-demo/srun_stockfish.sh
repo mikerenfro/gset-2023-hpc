083f89cd21e62b23d1995d68f99b3de12f811449
Mike Renfro <renfro@tntech.edu>
2023-06-22 13:40:47 -0500

    fix srun_stockfish to run on student HPC

--- chess-demo/srun_stockfish.sh
+++ chess-demo/srun_stockfish.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-srun --nodes=$1 --cpus-per-task=$2 ./stockfish
+#. /opt/ohpc/pub/spack/v0.20.0/share/spack/setup-env.sh
+#spack load openmpi
+#srun --nodes=$1 --cpus-per-task=$2 ./stockfish
+salloc --nodes=$1 --ntasks-per-node=$2 mpirun -np $(($1 * $2)) ./stockfish
